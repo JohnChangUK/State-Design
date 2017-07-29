89377ef89b7a3cebd4126ab3c8e5747c052329b1
John Chang <johnchang9094@gmail.com>
2017-07-29 12:03:19 +0100

    HasCard class implements ATMState

--- src/main/java/com/johnchang/ATMMachine.java
+++ src/main/java/com/johnchang/ATMMachine.java
@@ -18,6 +18,41 @@ public class ATMMachine {
         noCard = new noCard(this);
         hasCorrectPin = new hasCorrectPin(this);
         atmOutOfMoney = new atmOutOfMoney(this);
+
+        atmState = noCard;
+
+        if (cashInMachine < 0) {
+            atmState = atmOutOfMoney;
+        }
     }
+
+    void setATMState(ATMState newATMState) {
+        atmState = newATMState;
+    }
+
+    public void setCashInMachine(int newCashInMachine) {
+        cashInMachine = newCashInMachine;
+    }
+
+    public void insertCard() {
+        atmState.insertCard();
+    }
+
+    public void ejectCard() {
+        atmState.ejectCard();
+    }
+
+    public void requestCash(int cashToWithdraw) {
+        atmState.requestCash(cashToWithdraw);
+    }
+
+    public void insertPin(int pinEntered) {
+        atmState.insertPin(pinEntered);
+    }
+
+    public ATMState getYesCardState() { return hasCard; }
+    public ATMState getNoCardState() { return noCard; }
+    public ATMState getHasPin() { return hasCorrectPin; }
+    public ATMState getNoCashState() { return atmOutOfMoney; }
 }
 
--- src/main/java/com/johnchang/HasCard.java
+++ src/main/java/com/johnchang/HasCard.java
@@ -1,4 +1,26 @@
 package com.johnchang;
 
-public class HasCard {
+public class HasCard implements ATMState {
+
+    ATMMachine atmMachine;
+
+    public HasCard(ATMMachine newATMMachine) {
+        atmMachine = newATMMachine;
+    }
+
+    public void insertCard() {
+        System.out.println("You can't enter more than One card");
+    }
+
+    public void ejectCard() {
+
+    }
+
+    public void insertPin(int pinEntered) {
+
+    }
+
+    public void requestCash(int cashToWithdraw) {
+
+    }
 }
