4e96808f4870e87bcd5d35730a6d4e386a81f3a2
John Chang <johnchang9094@gmail.com>
2017-07-29 12:36:01 +0100

    Test State Machine ATM

--- src/main/java/com/johnchang/NoCash.java
+++ src/main/java/com/johnchang/NoCash.java
@@ -1,4 +1,31 @@
 package com.johnchang;
 
-public class NoCash {
+public class NoCash implements ATMState {
+
+    ATMMachine atmMachine;
+
+    public NoCash(ATMMachine newATMMachine) {
+
+        atmMachine = newATMMachine;
+    }
+
+    public void insertCard() {
+
+        System.out.println("Currently do not have any Money");
+    }
+
+    public void ejectCard() {
+
+        System.out.println("No Money available, Card not Entered");
+    }
+
+    public void insertPin(int pinEntered) {
+
+        System.out.println("Do not have money available");
+    }
+
+    public void requestCash(int cashToWithdraw) {
+
+        System.out.println("No Money Available");
+    }
 }
--- src/main/java/com/johnchang/TestATMMachine.java
+++ src/main/java/com/johnchang/TestATMMachine.java
@@ -1,4 +1,23 @@
 package com.johnchang;
 
 public class TestATMMachine {
+
+    public static void main(String[] args) {
+
+        ATMMachine atmMachine = new ATMMachine();
+
+        atmMachine.insertCard();
+
+        atmMachine.ejectCard();
+
+        atmMachine.insertCard();
+
+        atmMachine.insertPin(2345);
+
+        atmMachine.requestCash(2000);
+
+        atmMachine.insertCard();
+
+        atmMachine.insertPin(2345);
+    }
 }
